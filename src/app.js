2692600ba1390a68c64f92343de0092141b0caf5
Amanmandahad <mandahadaman>
2024-10-20 18:09:53 +0530

    last changes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,13 @@ const express = require('express')
 
 const app = express()
 
-app.use("/",(req,res)=>{
-    res.send("Hello, Worldsss!")
+app.use("/test",(req,res)=>{
+    res.send("Hello, test!")
 })
+app.use("/",(req,res)=>{
+    res.send("Hello, Worldeee!")
+})
+
+
 
 app.listen(7777)
