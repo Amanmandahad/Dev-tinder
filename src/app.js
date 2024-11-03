00d2ec2c4b77d2493d10b1ea4a080bbdb5dfa43c
Amanmandahad <mandahadaman>
2024-11-03 21:35:54 +0530

    Made an databse

--- src/app.js
+++ src/app.js
@@ -2,18 +2,23 @@ const express = require('express');
 require('./config/database')
 const app = express();
 const connectDb = require('./config/database');
+const User = require('./models/user')
 
-
-
-
-
-
-
-
-
-
-
-
+app.post('/signup', async (req, res) => {
+    // Creating a new instance
+    const user=  new User ({
+        firstName : "Aman",
+        lastName : "Mandahad",
+        emailId : "aman@gmail.com",
+        password : "1234566"
+    }) 
+  try{
+    await  user.save()
+    res.send('user added successfullyyy')
+  }catch(err){
+    res.status(400).send({"error": err})
+  }
+})
 
 
 
